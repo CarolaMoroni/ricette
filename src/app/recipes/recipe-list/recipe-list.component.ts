40f5c7844c5b08dccf81b9358fdff5723a874ad3
Carola <moronicarola@gmail.com>
2018-12-04 17:22:17 +0100

    modifiche ok

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  ricetta: Recipe = new Recipe(
+    'Patate e cozze',
+    'Lorem Ipsum',
+    'http://loremflickr.com/240/320/food'
+  );
+
+  ricetta2: Recipe = new Recipe(
+    'Pollo al curry',
+    'Lorem Ipsum',
+    'http://loremflickr.com/240/320/pollo'
+  );
+
+  ricetta3: Recipe = new Recipe(
+    'Riso alle vongole',
+    'Lorem Ipsum',
+    'http://loremflickr.com/240/320/riso'
+  );
+
+  recipes: Recipe[] = [this.ricetta, this.ricetta2, this.ricetta3];
+
+
+
+
+
+
 
   constructor() { }
 
